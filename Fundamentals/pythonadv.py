48f40683d6d1b6f724e964d1f401ffcccc887345
aswinpushkar <aswinpushkar11@gmail.com>
2025-01-23 17:36:50 +0530

    nested class

--- Fundamentals/pythonadv.py
+++ Fundamentals/pythonadv.py
@@ -1,31 +1,18 @@
-class Movie:
-    ''' This class gives Movie details'''
-    def __init__(self,title,hero,heroine):
-        self.title = title
-        self.hero = hero
-        self.heroine = heroine
-    def info(self):
-        
-        print('Movie Name:',self.title)
-        print('Hero Name:',self.hero)
-        print('Heroine Name:',self.heroine)
-    
+class University:
+    def __init__(self):
+        print('Outer class object created')
 
+    class Department:
+        def __init__(self):
+            print('Inner class object created')
+        def m1(self):
+            print('Calling Inner method')
 
-print(Movie.__doc__)
-movie_list=[]
-while True:
-    print('Enter the following details')
-    title=input('Movie Name:')
-    hero=input('Hero:')
-    heroine=input('heroine:')
-    m=Movie(title,hero,heroine)
-    movie_list.append(m)
-    print('Movie added successfully')
-    option=input('Do you want to add more movies?[Yes/No]')
-    if option.lower()=='no' and option.lower()!='yes':
-        break
-print('Movie Details are:')
-for m in movie_list:
-    m.info()
-    print()
+# u = University()
+# d = u.Department()
+# d.m1()
+
+# d=University().Department()
+# d.m1()
+
+University().Department().m1()
